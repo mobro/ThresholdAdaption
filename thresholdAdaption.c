961d52b0b53867307c22918ba363d59e11e72c51
mobro <mobro@users.noreply.github.com>
2019-09-27 18:13:52 +0200

    Update thresholdAdaption.c
    update

--- thresholdAdaption.c
+++ thresholdAdaption.c
@@ -1,30 +1,190 @@
-#include "thresholdAdapt.h"
-#include <stdint.h>
-#include <errno.h>
-#include <assert.h>
-#include <stdlib.h>
 
-uint16_t AdaptThreshold(uint32_t u32Position, uint32_t u32Offset, uint32_t u32MeasRange, uint32_t *pu32Threshold)
+
+// Includes *************************************************
+
+  #include "stdafx.h"
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "thresholdAdaption.h"
+
+// Defines **************************************************
+
+
+// Prototypes ************************************************
+static void assert(bool oParameter);
+
+// Variables ************************************************
+
+
+// Functions *************************************************
+//! 
+//! 
+//! 
+//!
+//! @return  
+uint16_t InitThreshold(STTHRESHOLD *pstThreshold, uint32_t u32MeasRange)
 {
-  uint32_t u32PosDiff = 0;
-  uint32_t u32PositionThreshold = 0;
+  uint8_t u8Index = 0;
   uint16_t u16Return = 0;
 
-  assert(NULL != pu32Threshold);
+  assert(NULL != pstThreshold);
 
-  u32PositionThreshold = u32Position + u32Offset;
-
-  if (0 != u32MeasRange)
+  if (NULL != pstThreshold)
   {
-    if (u32PositionThreshold >= u32MeasRange)
+    pstThreshold->u32MeasRange = u32MeasRange;
+
+    if (0 != pstThreshold->u32MeasRange)
     {
-      u32PositionThreshold %= u32MeasRange;
+      for (u8Index = 0; NUMOFTHRESHOLDS > u8Index; u8Index++)
+      {
+        pstThreshold->stThresholdElement[u8Index].oOverflow = false;
+        pstThreshold->stThresholdElement[u8Index].oTriggered = false;
+        pstThreshold->stThresholdElement[u8Index].u32Offset = 0;
+        pstThreshold->stThresholdElement[u8Index].u32StartPosition = 0;
+        pstThreshold->stThresholdElement[u8Index].u32Threshold = 0;
+      }
+
+      pstThreshold->oInitialized = true;
+    }
+    else
+    {
+      u16Return = EINVAL;
     }
   }
   else
   {
-    u16Return = EINVAL;
+    u16Return = EPERM;
   }
-  
+
   return u16Return;
+}
+
+
+//! 
+//! Adapt the position value to the meas range.
+//! Update the start position and the offset value before calling this function.
+//!
+//! @return  
+uint16_t SetThreshold(STTHRESHOLD *pstThreshold, ETHRESHOLD eThresholdNumber)
+{
+  uint32_t u32PositionThreshold = 0;
+  uint16_t u16Return = 0;
+
+  assert(NULL != pstThreshold);
+  assert(NUMOFTHRESHOLDS < eThresholdNumber);
+  
+
+  if ((false != pstThreshold->oInitialized) && (NULL != pstThreshold) && (NUMOFTHRESHOLDS > eThresholdNumber))
+  {
+    if ((0 != pstThreshold->u32MeasRange) && (pstThreshold->stThresholdElement[eThresholdNumber].u32Offset < pstThreshold->u32MeasRange))
+    {
+      u32PositionThreshold = pstThreshold->stThresholdElement[eThresholdNumber].u32StartPosition + pstThreshold->stThresholdElement[eThresholdNumber].u32Offset;
+
+      if (u32PositionThreshold >= pstThreshold->u32MeasRange)
+      {
+        u32PositionThreshold %= pstThreshold->u32MeasRange;
+        pstThreshold->stThresholdElement[eThresholdNumber].oOverflow = true;
+      }
+
+      pstThreshold->stThresholdElement[eThresholdNumber].u32Threshold = u32PositionThreshold;
+    }
+    else
+    {
+      u16Return = EINVAL;
+    }
+  }
+  else
+  {
+    u16Return = EPERM;
+  }
+
+  return u16Return;
+}
+
+//! 
+//! 
+//! 
+//!
+//! @return  
+uint16_t CheckThresholdTriggerd(STTHRESHOLD *pstThreshold, ETHRESHOLD eThresholdNumber, uint32_t u32ActPosition)
+{
+  uint16_t u16Return = 0;
+
+  assert(NULL != pstThreshold);
+  assert(NUMOFTHRESHOLDS < eThresholdNumber);
+
+  if (NULL != pstThreshold)
+  {
+    if ((false != pstThreshold->oInitialized) && (NUMOFTHRESHOLDS > eThresholdNumber))
+    {
+      if (true != pstThreshold->stThresholdElement[eThresholdNumber].oOverflow)
+      {
+        if (u32ActPosition >= pstThreshold->stThresholdElement[eThresholdNumber].u32Threshold)
+        {
+          pstThreshold->stThresholdElement[eThresholdNumber].oTriggered = true;
+        }
+      }
+      else
+      {
+        if (u32ActPosition < pstThreshold->stThresholdElement[eThresholdNumber].u32Threshold)
+        {
+          pstThreshold->stThresholdElement[eThresholdNumber].oOverflow = false;
+        }
+      }
+    }
+    else
+    {
+      u16Return = EPERM;
+    }
+  }
+  else
+  {
+    u16Return = EPERM;
+  }
+
+  return u16Return;
+}
+
+//! 
+//! 
+//! 
+//!
+//! @return  
+uint16_t ResetThresholdTriggerd(STTHRESHOLD *pstThreshold, ETHRESHOLD eThresholdNumber)
+{
+  uint16_t u16Return = 0;
+
+  assert(NULL != pstThreshold);
+  assert(NUMOFTHRESHOLDS < eThresholdNumber);
+
+  if (NULL != pstThreshold)
+  {
+    if ((false != pstThreshold->oInitialized) && (NUMOFTHRESHOLDS > eThresholdNumber))
+    {
+      pstThreshold->stThresholdElement[eThresholdNumber].oTriggered = false;
+    }
+    else
+    {
+      u16Return = EPERM;
+    }
+  }
+  else
+  {
+    u16Return = EPERM;
+  }
+
+
+  return u16Return;
+}
+
+
+
+// --- static functions ---
+static void assert(bool oParameter)
+{
+  
 }
